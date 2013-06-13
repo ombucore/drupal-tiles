c7b9a9e3587ced77d1f18588f239cf460b13989c
Jon Duell <jon@jonduell.com>
2013-06-12 19:20:31 -0700

    Adds initial tiles container
    Ported over from bean_container (https://github.com/ombu/drupal-beancontainer)
    
    refs #4

---
+++ modules/tiles_container/README.md
@@ -0,0 +1,7 @@
+Bean Container
+==============
+A bean that contains other beans.
+
+The basis of tiles container is just what it sounds like, a bean (block) that
+contains an arbitrary number of other beans.  It then displays those children
+beans in a tabbed panel.
---
+++ modules/tiles_container/includes/TilesContainer.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @file
+ * Bean plugin object for bean container.
+ */
+
+class BeanContainer extends ombubeans_color {
+  /**
+   * @see: parent::values().
+   */
+  public function values() {
+    $values = parent::values();
+
+    $values['children'] = array();
+    $values['display_type'] = 'simple';
+
+    return $values;
+  }
+
+  /**
+   * @see: parent::form().
+   */
+  public function form($bean, $form, &$form_state) {
+    $form = parent::form($bean, $form, $form_state);
+
+    $form['children'] = array(
+      '#type' => 'value',
+      '#value' => $bean->children,
+    );
+
+    // @todo: make this pluggable.
+    $form['display_type'] = array(
+      '#type' => 'select',
+      '#title' => t('Display type'),
+      '#options' => array(
+        'simple' => t('Invisible Container'),
+        'tab' => t('Tabbed Panel'),
+      ),
+      '#default_value' => $bean->display_type,
+    );
+
+    return $form;
+  }
+
+  /**
+   * Return the block content.
+   *
+   * @param $bean
+   *   The bean object being viewed.
+   * @param $content
+   *   The default content array created by Entity API.  This will include any
+   *   fields attached to the entity.
+   * @param $view_mode
+   *   The view mode passed into $entity->view().
+   * @return
+   *   Return a renderable content array.
+   */
+  public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
+    $children = array();
+
+    if (!empty($bean->children)) {
+      $i = 1;
+      foreach ($bean->children as $child) {
+        $child_bean = bean_load($child);
+        if ($child_bean && bean_access('view', $child_bean)) {
+          $children[] = $child_bean;
+        }
+      }
+    }
+
+    // @todo: implement different themes based on display_type.
+    $content['bean'][$bean->bid]['children'] = array(
+      '#theme' => 'tiles_container',
+      '#children' => $children,
+      '#display_type' => $bean->display_type,
+      '#parent' => $bean,
+    );
+
+    return $content;
+  }
+}
