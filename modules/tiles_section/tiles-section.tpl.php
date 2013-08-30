9fbebb88e215bfd5241d8446df1242ec8ba5288e
Adam Page <adamfpage@gmail.com>
2013-08-30 16:25:56 -0700

    Include a "Return to top" link in each tile section.

--- modules/tiles_section/tiles-section.tpl.php
+++ modules/tiles_section/tiles-section.tpl.php
@@ -16,5 +16,6 @@
   <?php print render($title_suffix) ?>
   <div class="content" data-name="content" data-type="section" data-context="<?php print $context_id ?>">
     <?php print render($tiles) ?>
+    <p class="top-link"><a href="#wrap">Back to top</a></p>
   </div>
 </div>
