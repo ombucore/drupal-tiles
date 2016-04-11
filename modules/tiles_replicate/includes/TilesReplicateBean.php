4304c2c72a8596982024cc63f02b669586399546
Jon Duell <jon@jonduell.com>
2016-04-10 17:09:43 -0700

    Speeds up replicate of beans by skipping cache clearing

---
+++ modules/tiles_replicate/includes/TilesReplicateBean.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Override for Bean class.
+ *
+ * Exists mainly to prevent cache clearing during bean replicate, which can be a
+ * huge performance hit.
+ */
+
+class TilesReplicateBean extends Bean {
+  /**
+   * Implements parent::save().
+   *
+   * Since multiple beans are replicated at the same time (e.g. during a node
+   * replication that contains many tile sections), there's no need to do a block
+   * rehash every time. That can be done instead at the end of the replication
+   * process.
+   */
+  public function save() {
+    if (FALSE && !empty($this->replicated)) {
+      $this->setUid()->checkDelta();
+
+      if (empty($this->created)) {
+        $this->created = REQUEST_TIME;
+      }
+
+      $this->changed = REQUEST_TIME;
+
+      $this->plugin->submit($this);
+
+      bean_reset();
+
+      return entity_get_controller($this->entityType)->save($this);
+    }
+    else {
+      return parent::save();
+    }
+  }
+}
